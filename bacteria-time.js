5c39b48886fe820346f4da9dda73283a603fa27a
Tdask <45751838+Tdask@users.noreply.github.com>
2019-01-13 00:02:05 -0500

    Delete bacteria-time.js

--- bacteria-time.js
+++
@@ -1,8 +0,0 @@
-// YOUR CODE BELOW
-const bacteriaTime = (currentNum, targetNum) => {
-    if (targetNum < currentNum) return 'targetNum must be larger than currentNum'
-    let minCount = 0;
-    for (let i = currentNum; i < targetNum; i *= 2){
-        minCount += 20;
-    } return minCount;
-}
