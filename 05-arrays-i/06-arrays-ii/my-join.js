8e19ba18826aef367564ac4cac9e3ff43e8f7c8a
Tdask <45751838+Tdask@users.noreply.github.com>
2019-01-13 02:05:37 -0500

    Delete my-join.js

--- 05-arrays-i/06-arrays-ii/my-join.js
+++
@@ -1,9 +0,0 @@
-// YOUR CODE BELOW
-function myJoin(arr, sep = ','){
-    let newStr = '';
-    for (let i = 0; i < arr.length; i++){
-        if (i === arr.length -1) newStr += arr[i];
-        else if (arr[i] === null || undefined) newStr += sep;
-        else newStr += arr[i] + sep;       
-    } return newStr;
-}
