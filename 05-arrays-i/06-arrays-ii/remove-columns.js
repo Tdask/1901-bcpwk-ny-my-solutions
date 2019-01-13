98c43267d94aadba123e84c41caacea7edd358ef
Tdask <45751838+Tdask@users.noreply.github.com>
2019-01-13 02:05:53 -0500

    Delete remove-columns.js

--- 05-arrays-i/06-arrays-ii/remove-columns.js
+++
@@ -1,8 +0,0 @@
-// YOUR CODE BELOW
-
-const removeColumns = (originalGrid, numColumns) =>{
-    for (let i = 0; i < originalGrid.length; i++){
-      originalGrid[i].splice(originalGrid[i].length - numColumns, numColumns)
-    }
-     return originalGrid;
-  }
