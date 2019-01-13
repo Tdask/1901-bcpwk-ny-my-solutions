bd6684a74801d6d9295ef10ce0dc40899d114fa2
Tdask <45751838+Tdask@users.noreply.github.com>
2019-01-13 00:03:09 -0500

    Delete my-slice.js

--- my-slice.js
+++
@@ -1,7 +0,0 @@
-// YOUR CODE BELOW
-const mySlice = (originalStr, startIdx = 0, endIdx = originalStr.length) => {
-    let newStr = '';
-    for (let i = startIdx; i < endIdx; i++ ){
-        newStr += originalStr[i];
-    } return newStr;
-}
