468bae8b13357f4da07b988dcae61758fd85989d
Tdask <45751838+Tdask@users.noreply.github.com>
2019-01-13 00:02:13 -0500

    Delete crazy-caps.js

--- crazy-caps.js
+++
@@ -1,8 +0,0 @@
-// YOUR CODE BELOW
-const crazyCaps = str =>{
-let newStr = ''    
-for (let i = 0; i < str.length; i++){
-    if (i % 2 === 0) newStr += str[i]
-    else newStr += str[i].toUpperCase();
-  } return newStr;
-}
