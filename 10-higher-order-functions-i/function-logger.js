96b9c59cae295094be5d3bdf2d23e0d2e2764cf7
Tdask <45751838+Tdask@users.noreply.github.com>
2019-01-13 10:19:52 -0500

    Add files via upload

---
+++ 10-higher-order-functions-i/function-logger.js
@@ -0,0 +1,8 @@
+
+// YOUR CODE BELOW
+function functionLogger(func, arg){
+    console.log('Function starting')
+    let result = func(arg)
+    console.log('Function complete')
+    return result;
+}
---
+++ 10-higher-order-functions-i/my-for-each.js
@@ -0,0 +1,7 @@
+
+// YOUR CODE BELOW
+function myForEach(arr, callback){
+    for (let i = 0; i <arr.length; i++){
+        callback (arr[i], i)
+    }
+}
